ce9da44cf7487cbcd7dcee25cede39a27b317ab6
Jiexi-Ash <lance.jiexi@gmail.com>
2026-03-25 19:09:47 +0200

    create a footer component and move footer content to that component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,40 +1,26 @@
-import { getAuthToken } from '@/auth'
-import DashboardContent from '@/components/dashboard/dashboardContent';
-import { api } from '@/convex/_generated/api';
+import { getAuthToken } from "@/auth";
+import DashboardContent from "@/components/dashboard/dashboardContent";
+import DashboardFooter from "@/components/dashboard/footer";
+import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
-import { CalendarDays, LayoutGrid, Scissors, Users } from 'lucide-react';
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 async function DashboardPage() {
-    const token = await getAuthToken();
-    const business = await fetchQuery(api.business.admin.getUserBusiness, {}, { token })
+  const token = await getAuthToken();
+  const business = await fetchQuery(
+    api.business.admin.getUserBusiness,
+    {},
+    { token },
+  );
 
-    if (!business) redirect("/onboarding")
-    return (
-        <div className="w-full min-h-screen justify-center items-center bg-[#F5F5F5]">
-            <DashboardContent business={business} />
+  if (!business) redirect("/onboarding");
+  return (
+    <div className="w-full min-h-screen justify-center items-center bg-[#F5F5F5]">
+      <DashboardContent business={business} />
 
-            <footer className="sticky bottom-0  border-t border-border grid grid-cols-4 gap-4 p-6">
-                <div className="flex flex-col gap-1 items-center text-primary bg-white py-2 px-4 rounded-md shadow-sm">
-                    <LayoutGrid className="size-6" strokeWidth={1.5} />
-                    <span className="text-xs font-semibold">OVERVIEW</span>
-                </div>
-                <div className="flex flex-col gap-1 items-center py-2 px-4 text-gray-500">
-                    <CalendarDays className="size-6 text-gray-500" />
-                    <span className="text-xs text-gray-500">Bookings</span>
-                </div>
-                <div className="flex flex-col gap-1 items-center py-2 px-4">
-                    <Scissors className="size-6 text-gray-500" />
-                    <span className="text-xs text-gray-500">Services</span>
-                </div>
-
-                <div className="flex flex-col gap-1 items-center py-2 px-4">
-                    <Users className="size-6 text-gray-500" />
-                    <span className="text-xs text-gray-500">Bookings</span>
-                </div>
-            </footer>
-        </div>
-    )
+      <DashboardFooter />
+    </div>
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
---
+++ components/dashboard/footer.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { cn } from "@/lib/utils";
+import {
+  CalendarDays,
+  LayoutGrid,
+  type LucideIcon,
+  Scissors,
+  Users,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navlinks = [
+  { href: "/dashboard", label: "OVERVIEW", icon: LayoutGrid },
+  { href: "/dashboard/bookings", label: "BOOKING", icon: CalendarDays },
+  { href: "/dashboard/services", label: "SERVICES", icon: Scissors },
+  { href: "/dashboard/clients", label: "CLIENTS", icon: Users },
+];
+function DashboardFooter() {
+  const pathname = usePathname();
+  return (
+    <footer className="sticky bottom-0 lg:hidden  border-t border-border grid grid-cols-4 gap-4 p-6">
+      {navlinks.map((link) => (
+        <Navlink
+          key={link.label}
+          Icon={link.icon}
+          label={link.label}
+          href={link.href}
+          isActive={pathname.includes(link.href)}
+        />
+      ))}
+    </footer>
+  );
+}
+
+export default DashboardFooter;
+
+const Navlink = ({
+  label,
+  Icon,
+  href,
+  isActive,
+}: {
+  label: string;
+  href: string;
+  Icon: LucideIcon;
+  isActive: boolean;
+}) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "flex flex-col gap-1 py-4 px-4  items-center",
+        isActive ? "text-primary " : "text-gray-500",
+      )}
+    >
+      <Icon className="size-6" strokeWidth={1.5} />
+      <span className="text-xs font-semibold">{label}</span>
+    </Link>
+  );
+};
