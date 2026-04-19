4ac0eeab12ce421eb6bae8320c230a03a14a727f
Lance <lance.jiexi@gmail.com>
2026-04-19 13:46:36 +0200

    add explore page with fetch from convex/business/public

---
+++ app/explore/page.tsx
@@ -0,0 +1,22 @@
+
+import { getAuthToken } from '@/auth'
+import Explore from '@/components/explore'
+import Navbar from '@/components/navbar'
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { preloadQuery } from 'convex/nextjs'
+import Link from 'next/link'
+import React from 'react'
+
+async function ExplorePage() {
+    const token = await getAuthToken()
+    const preloadBusinesses = await preloadQuery(api.business.public.getBusinesses, { limit: 12 }, { token })
+    return (
+        <div className="w-full min-h-screen bg-stone-50">
+            <Navbar />
+            <Explore preloadedBusinesses={preloadBusinesses} />
+        </div>
+    )
+}
+
+export default ExplorePage
