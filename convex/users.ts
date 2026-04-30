29f47bf9b2b4c1d557da2c27b909feaf1bac615c
Lance <lance.jiexi@gmail.com>
2026-04-30 12:00:11 +0200

    update: users functions

--- convex/users.ts
+++ convex/users.ts
@@ -2,56 +2,54 @@ import { internalMutation, query, QueryCtx } from "./_generated/server";
 import { UserJSON } from "@clerk/backend";
 import { ConvexError, v, Validator } from "convex/values";
 
-
-
-
-
-
 export const upsertFromClerk = internalMutation({
-    args: {
-        data:v.any() as Validator<UserJSON>
-    },
-    handler: async (ctx, {data}) => {
-        const userData = {
-            fullname: `${data.first_name} ${data.last_name}`,
-            clerkId:data.id,
-            email:data.email_addresses[0].email_address,
-            image:data.image_url,
-            phone: (data.phone_numbers && data.phone_numbers.length > 0 && data.phone_numbers[0]?.phone_number) || undefined
-        }
-      
+  args: {
+    data: v.any() as Validator<UserJSON>,
+  },
+  handler: async (ctx, { data }) => {
+    const userData = {
+      fullname: `${data.first_name} ${data.last_name}`,
+      clerkId: data.id,
+      email: data.email_addresses[0].email_address,
+      image: data.image_url,
+      phone:
+        (data.phone_numbers &&
+          data.phone_numbers.length > 0 &&
+          data.phone_numbers[0]?.phone_number) ||
+        undefined,
+    };
 
-        const user = await getUserByClerkId(ctx, userData.clerkId)
+    const user = await getUserByClerkId(ctx, userData.clerkId);
 
-        if (!user || user === null) {
-            await ctx.db.insert("users", userData)
-            
-        } else {
-
-            await ctx.db.patch(user._id, userData)
-        }
-
+    if (!user || user === null) {
+      await ctx.db.insert("users", userData);
+    } else {
+      await ctx.db.patch(user._id, userData);
     }
-})
+  },
+});
 
-const getUserByClerkId = async (ctx:QueryCtx, clerkId:string) => {
-    return await ctx.db.query("users").withIndex("by_clerk_id", q => q.eq("clerkId", clerkId)).unique()
-}
+const getUserByClerkId = async (ctx: QueryCtx, clerkId: string) => {
+  return await ctx.db
+    .query("users")
+    .withIndex("by_clerk_id", (q) => q.eq("clerkId", clerkId))
+    .unique();
+};
 
-export const getCurrentUserOrThrow = async (ctx:QueryCtx) => {
-    const user =  await getCurrentUser(ctx)
+export const getCurrentUserOrThrow = async (ctx: QueryCtx) => {
+  const user = await getCurrentUser(ctx);
 
-    if (!user) throw new ConvexError("User not found")
+  if (!user) throw new ConvexError("User not found");
 
-    return user
-}
+  return user;
+};
 
-export const getCurrentUser = async (ctx:QueryCtx) => {
-    const identity = await ctx.auth.getUserIdentity()
+export const getCurrentUser = async (ctx: QueryCtx) => {
+  const identity = await ctx.auth.getUserIdentity();
 
-    if (identity === null) {
-        return null
-    }
+  if (identity === null) {
+    return null;
+  }
 
-    return await getUserByClerkId(ctx, identity.subject)
-}
+  return await getUserByClerkId(ctx, identity.subject);
+};
