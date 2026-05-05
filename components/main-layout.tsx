fd8d7f89b45b1a667297a037c7d7d4561250ad1b
Lance <lance.jiexi@gmail.com>
2026-05-05 17:21:34 +0200

    style:remove padding

--- components/main-layout.tsx
+++ components/main-layout.tsx
@@ -4,7 +4,7 @@ function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen bg-stone-50 flex flex-col">
             <Navbar />
-            <main className="flex-1 w-full mx-auto px-6 py-4 max-w-[1440px]">
+            <main className="flex-1 w-full mx-auto py-4 max-w-[1440px]">
                 {children}
             </main>
         </div>
