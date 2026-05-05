8867fca006fe9b029f742f1337bedf4aeb54a140
Lance <lance.jiexi@gmail.com>
2026-05-05 17:22:49 +0200

    add profile directory (unfinished)

---
+++ app/profile/bookings/page.tsx
@@ -0,0 +1,17 @@
+import { getAuthToken } from '@/auth'
+import UserBookings from '@/components/user-bookings'
+import { api } from '@/convex/_generated/api'
+import { preloadQuery } from 'convex/nextjs'
+import React from 'react'
+
+async function UserBookingsPage() {
+
+    const token = await getAuthToken()
+
+    const preloadBookings = await preloadQuery(api.booking.user.getUserBookings, {}, { token })
+    return (
+        <UserBookings preloadedBookings={preloadBookings} />
+    )
+}
+
+export default UserBookingsPage
