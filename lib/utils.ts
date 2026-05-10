0ba4dc3d064fc1e48dfadfcf4fdbe5bb09bb1cd0
Lance <lance.jiexi@gmail.com>
2026-05-10 13:40:44 +0200

    add more helper funcs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
+import { TZDate } from "@date-fns/tz";
 import { clsx, type ClassValue } from "clsx";
+import { format } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +15,28 @@ export const slugify = (name: string) => {
     .trim()
     .replace(/\s+/g, "-")
     .replace(/-+/g, "-");
+};
+
+export const formatBookingShortDate = (
+  timestamp: number,
+  timezone?: string,
+) => {
+  const zonedDate = timezone
+    ? new TZDate(timestamp, timezone)
+    : new Date(timestamp);
+  return format(zonedDate, "MMM d, p");
+};
+
+export const formatBookingDate = (timestamp: number, timezone?: string) => {
+  const zonedDate = timezone
+    ? new TZDate(timestamp, timezone)
+    : new Date(timestamp);
+  return format(zonedDate, "MMM d, yyyy");
+};
+
+export const formatBookingTime = (timestamp: number, timezone?: string) => {
+  const zonedDate = timezone
+    ? new TZDate(timestamp, timezone)
+    : new Date(timestamp);
+  return format(zonedDate, "p"); // e.g. "2:30 PM"
 };
