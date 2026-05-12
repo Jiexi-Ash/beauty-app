4c5eae574df661739aa383b1e6afaa04eabe62ae
Lance <lance.jiexi@gmail.com>
2026-05-12 16:18:54 +0200

    feat: add getUserProfileDetails and updateUserPhoneNumber functions

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,9 @@
-import { internalMutation, query, QueryCtx } from "./_generated/server";
+import {
+  internalMutation,
+  mutation,
+  query,
+  QueryCtx,
+} from "./_generated/server";
 import { UserJSON } from "@clerk/backend";
 import { ConvexError, v, Validator } from "convex/values";
 
@@ -53,3 +58,43 @@ export const getCurrentUser = async (ctx: QueryCtx) => {
 
   return await getUserByClerkId(ctx, identity.subject);
 };
+
+export const getUserProfileDetails = query({
+  handler: async (ctx) => {
+    const user = await getCurrentUser(ctx);
+
+    if (!user) return null;
+
+    const bookings = await ctx.db
+      .query("booking")
+      .withIndex("by_user", (q) => q.eq("userId", user._id))
+      .collect();
+
+    return {
+      user: user,
+      totalBookings: bookings.length,
+      completedBookings: bookings.filter((b) => b.status === "completed")
+        .length,
+      upcomingBookings: bookings.filter((b) => b.status === "upcoming").length,
+      cancelledBookings: bookings.filter(
+        (b) => b.status === "cancelled_by_user",
+      ).length,
+    };
+  },
+});
+
+export const updateUserPhoneNumber = mutation({
+  args: {
+    phoneNumber: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await getCurrentUser(ctx);
+
+    if (!user)
+      throw new ConvexError("You need to be logged in to perform this action.");
+
+    await ctx.db.patch(user._id, {
+      phone: args.phoneNumber,
+    });
+  },
+});
